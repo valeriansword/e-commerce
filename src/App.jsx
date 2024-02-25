632ae1254e758001c04bede009d88ccfd58b76b8
valeriansword <chandrakumarxii@gmail.com>
2024-02-25 19:54:46 +0530

    added e-commerce

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from "./Header";
+function App(){
+  return(
+    <div>
+      <Header />
+    </div>
+  )
+}
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,25 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+function Header(){
+    return(
+    <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+     </ul>
+        </div>
+        </div>
+        
+    </nav>
+    )
+}
+export default Header;
