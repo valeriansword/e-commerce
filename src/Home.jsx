c4ca8ce3176f262943b94af4816eaaa9992a77f7
valeriansword <chandrakumarxii@gmail.com>
2024-03-18 17:19:21 +0530

    added

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ function Home(props){
         <div className="min-vh-100 row d-flex justify-content-center">
              { props.product.map((item)=>(
             
-            <div key={item.id} className="card w-25 m-2 rounded-0  col-lg-3" >
+            <div key={item.id} id="homeCard" className="card  m-2 rounded-0  col-lg-3 col-sm-6" >
                  <img src={item.thumbnail} id="image" className="card-img-top  rounded-0 " style={{height:"200px"}} alt="..." />
                  <div className="card-body">
                       <h5 className="card-title">{item.title}</h5>
